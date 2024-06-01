583fba12189d83e3314a969c656c3d43b2c249fa
snekcode <code.snakes@gmail.com>
2024-05-31 20:44:00 -0500

    switched to config file swapping instead of folder swapping

--- electron/GameManager/index.ts
+++ electron/GameManager/index.ts
@@ -1,29 +1,37 @@
 import fs, {} from 'fs'
 import { execFile } from 'child_process';
 import { EGameNames } from 'Shared/gamenames';
+import {app} from 'electron'
+
+const userData = app.getPath('userData')
 
 export const LOCAL_APP_DATA = process.env.LOCALAPPDATA;
 
 export const config = {
+    path: `${LOCAL_APP_DATA}\\TheIsle`,
     steam: {
       path: 'C:\\Program Files (x86)\\Steam',
+    },
+    controlFiles: {
+      settings: {
+        path: 'Saved\\Config\\WindowsClient\\GameUserSettings.ini',
+        savePath: `${userData}\\GameSettings`,
+        legacyFlag: '[/Script/TheIsle.IGameUserSettings]',
+        evrimaFlag: '[/Script/TheIsle.TIGameUserSettings]',
+        legacySaveName: 'LegacyGameUserSettings.ini',
+        evrimaSaveName: 'EvrimaGameUserSettings.ini'
+      },
     },
     legacy: {
       path: 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Isle Legacy',
       name: 'TheIsle.exe',
-      runtimeAppDataName: 'TheIsle',
-      thisStandbyAppDataName: 'TheIsleLegacy',
-      otherStandbyAppDataName: 'TheIsleEvrima',
-      installIndicator: 'Engine\\Extras\\Redist\\en-us\\UE4PrereqSetup_x64.exe',
+      installIndicator: 'Engine\\Extras\\Redist\\en-us\\UE4PrereqSetup_x64.exe'
+
     },
     evrima: {
       path: 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Isle',
       name: 'TheIsle\\Binaries\\Win64\\TheIsleClient-Win64-Shipping.exe',
-      runtimeAppDataName: 'TheIsle',
-      thisStandbyAppDataName: 'TheIsleEvrima',
-      otherStandbyAppDataName: 'TheIsleLegacy',
-      installIndicator: 'EasyAntiCheat',
-      isEvrimaAppDataFolderIndicator: 'Saved\\Config\\Windows\\ApexDestruction.ini',
+      installIndicator: 'EasyAntiCheat'
     }
 }
 
@@ -38,14 +46,14 @@ export function setUpLegacy(){
     
     fs.writeFileSync(`${config.legacy.path}\\TheIsle\\Binaries\\Win64\\steam_appid.txt`, '376210')
     // create a shortcut (symlink) named TheIsleLegacy on the OneDrive // desktop
-    checkInAppData(EGameNames.legacy);
+    // checkInAppData(EGameNames.legacy);
     return true
   }else if(checksForInstall(EGameNames.legacy)){
     if(fs.existsSync(`${config.legacy.path}\\TheIsle\\Binaries\\Win64\\steam_appid.txt`)){
       fs.writeFileSync(`${config.legacy.path}\\TheIsle\\Binaries\\Win64\\steam_appid.txt`, '376210')
     }
     // game is installed as legacy
-    checkInAppData(EGameNames.legacy);
+    // checkInAppData(EGameNames.legacy);
     return true
   }else {
     // game is not installed as legacy
@@ -53,65 +61,108 @@ export function setUpLegacy(){
   }
 }
 
-let attempts = 0;
-const maxAttempts = 10;
+// let attempts = 0;
+// const maxAttempts = 10;
 
-export function checkInAppData(name: EGameNames){
-    const game = config[name];
-    if(fs.existsSync(`${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`)){
-      return;
-    }
-    try{
-      fs.renameSync(`${LOCAL_APP_DATA}\\${game.runtimeAppDataName}`, `${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    }catch(e: any){
-      if(e.code === 'EPERM' && attempts < maxAttempts){
-        console.log('waiting for access', attempts);
-        attempts++;
-        setTimeout(() => {
-          checkInAppData(name);
-        }, 1000);
-    }else {
-      console.log("something went wrong checking in");
-    }
+// export function checkInAppData(name: EGameNames){
+//     const game = config[name];
+//     if(fs.existsSync(`${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`)){
+//       return;
+//     }
+//     try{
+//       fs.renameSync(`${LOCAL_APP_DATA}\\${game.runtimeAppDataName}`, `${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`);
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//     }catch(e: any){
+//       if(e.code === 'EPERM' && attempts < maxAttempts){
+//         console.log('waiting for access', attempts);
+//         attempts++;
+//         setTimeout(() => {
+//           checkInAppData(name);
+//         }, 1000);
+//     }else {
+//       console.log("something went wrong checking in");
+//     }
+//   }
+// }
+
+// export function checkOutAppData(name: EGameNames){
+//     const game = config[name];
+//     if(!fs.existsSync(`${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`)){
+//       return;
+//     }
+//     const standByFileName = `${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`
+//     const runTimeFileName = `${LOCAL_APP_DATA}\\${game.runtimeAppDataName}`
+//     try{
+
+//       fs.renameSync(standByFileName, runTimeFileName);
+//     }catch (e){
+//       console.log(standByFileName, runTimeFileName, e);
+      
+//       console.log("something went wrong checking out");
+//     }
+// }
+
+function testControlFile(game: EGameNames, name: keyof typeof config.controlFiles){
+
+  const controlFile = `${config.path}\\${config.controlFiles[name].path}`
+  if(!fs.existsSync(controlFile)){
+    return true;
   }
+  const data = fs.readFileSync(controlFile).toString()
+  // @ts-expect-error implicit string type error
+  return data.includes(config.controlFiles[name][game + "Flag"])
 }
 
-export function checkOutAppData(name: EGameNames){
-    const game = config[name];
-    if(!fs.existsSync(`${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`)){
-      return;
-    }
-    try{
-    fs.renameSync(`${LOCAL_APP_DATA}\\${game.thisStandbyAppDataName}`, `${LOCAL_APP_DATA}\\${game.runtimeAppDataName}`);
-    }catch{
-      console.log("something went wrong checking out");
-    }
+function saveControlFile(game: EGameNames, name: keyof typeof config.controlFiles){
+  console.log('saving', game, name);
+  
+  const path = `${config.path}\\${config.controlFiles[name].path}`
+  // @ts-expect-error implicit string type error
+  const newPath = `${config.controlFiles[name].savePath}\\${config.controlFiles[name][game + 'SaveName']}`
+  fs.copyFileSync(path, newPath)
+  return true;
+}
+
+function loadControlFile(game: EGameNames, name: keyof typeof config.controlFiles){
+  console.log('loading', game, name);
+
+  const path = `${config.path}\\${config.controlFiles[name].path}`
+  // @ts-expect-error implicit string type error
+  const newPath = `${config.controlFiles[name].savePath}\\${config.controlFiles[name][game + 'SaveName']}`
+  try{
+    fs.copyFileSync(newPath, path)
+  }catch{
+    fs.writeFileSync(path, "")
+  }
+  return true;
 }
 
 export function swapVersion(name: EGameNames){
   const currentGame = checkCurrentAppDataFolderType();
+  console.log(currentGame);
+  
   if(currentGame === "none"){
-    checkOutAppData(name);
+    // TODO Insert Control Files
+    loadControlFile(name, 'settings')
     return [true, name];
   }
 
   if( currentGame === name){
     return [true, name];
   }else{
-    checkInAppData(currentGame)
-    checkOutAppData(name);
+    // Swap Control Files
+    saveControlFile(currentGame, 'settings')
+    loadControlFile(name, 'settings')
+    return [true, name]
   }
-  const verifiedName = checkCurrentAppDataFolderType()
-  return [verifiedName === name, verifiedName];
 }
 
 export function checkCurrentAppDataFolderType(): EGameNames | "none" {
-  if(fs.existsSync(`${LOCAL_APP_DATA}\\${config.evrima.runtimeAppDataName}`)){
-      return fs.existsSync(`${LOCAL_APP_DATA}\\${config.evrima.runtimeAppDataName}\\${config.evrima.isEvrimaAppDataFolderIndicator}`) ? EGameNames.evrima : EGameNames.legacy;
-  }else {
-    return "none";
-  }
+  const isLegacy = testControlFile(EGameNames.legacy, "settings")
+  const isEvrima = testControlFile(EGameNames.evrima, "settings")
+  if(isLegacy && !isEvrima) return EGameNames.legacy
+  if(isEvrima && !isLegacy) return EGameNames.evrima
+  return "none"
 }
 
 
@@ -119,14 +170,14 @@ export function checksForInstall(name: EGameNames){
   return fs.existsSync(config[name].path) && fs.existsSync(`${config[name].path}\\${config[name].installIndicator}`)
 }
 
-export function checksForAppData(name: EGameNames){
-  const loadedGame = checkCurrentAppDataFolderType();
-  if(loadedGame === name){
-    return true;
-  }else{
-    return fs.existsSync(`${LOCAL_APP_DATA}\\${config[name].thisStandbyAppDataName}`)
-  }
-}
+// export function checksForAppData(name: EGameNames){
+//   const loadedGame = checkCurrentAppDataFolderType();
+//   if(loadedGame === name){
+//     return true;
+//   }else{
+//     return fs.existsSync(`${LOCAL_APP_DATA}\\${config[name].thisStandbyAppDataName}`)
+//   }
+// }
 
 export function startGame(name: EGameNames, args: string[]){
     const game = config[name];
--- electron/GameManager/setupConfig.ts
+++ electron/GameManager/setupConfig.ts
@@ -1,126 +1,44 @@
 import { store } from "~/electron/main/store";
 import {
   checkCurrentAppDataFolderType,
-  checkOutAppData,
-  checksForAppData,
+  // checkOutAppData,
+  // checksForAppData,
   checksForInstall,
-  LOCAL_APP_DATA,
 } from ".";
 import { EGameNames } from "~/Shared/gamenames";
 import fs from "fs";
-import path from "node:path";
+import {app} from 'electron'
+
+const userData = app.getPath('userData')
+
+// Added in Version 1.2.0 to create a folder in userData `AppData\Roaming\theislemanager\GameSettings` to store game config files
+if(!fs.existsSync(`${userData}\\GameSettings`)){
+  fs.mkdirSync(`${userData}\\GameSettings`)
+}
 
 const loadedGame = checkCurrentAppDataFolderType();
 const legacyInstall = checksForInstall(EGameNames.legacy);
 const evrimaInstall = checksForInstall(EGameNames.evrima);
-const legacyAppData = checksForAppData(EGameNames.legacy);
-const evrimaAppData = checksForAppData(EGameNames.evrima);
 
 store.set("loadedGame", loadedGame);
 store.set("legacyInstall", legacyInstall);
 store.set("evrimaInstall", evrimaInstall);
-store.set("legacyAppData", legacyAppData);
-store.set("evrimaAppData", evrimaAppData);
 
 // const DEBUG = store.get("DEBUG") ?? false;
 
-// let gameRunning = false;
-
-// const execCallback = (error: any, stdout: any, stderr: any) => {
-//     if(error && DEBUG){
-//       console.log(error);
-//       }
-//     if(stdout){
-//       console.log(stdout);
-//       setTimeout(() => gameRunning = false, 2000)
-//       }
-//     if(stderr && DEBUG){
-//       console.log(stderr);
-//       }
-//     }
-
-// const startGameForConfig = (name: EGameNames) => {
-//   const game = config[name];
-//   const gameExe = `${game.path}\\${game.name}`;
-//   const runningGame = execFile(gameExe);
-//   gameRunning = true;
-
-//   console.log("PID: ", runningGame.pid);
-
-  // while (gameRunning) {
-  //   if (fs.existsSync(`${LOCAL_APP_DATA}\\${game.runtimeAppDataName}\\Saved\\Config\\WindowsClient\\Engine.ini`)) {
-  //     exec('taskkill /IM TheIsleClient-Win64-Shipping.exe /F /T', execCallback) 
-  //     exec(`taskkill /IM TheIsle.exe /F /T`, execCallback);
-  //     exec('taskkill /PID ' + runningGame.pid + ' /T /F', execCallback);
-  //   }
-  // }
-
-  // setTimeout(() => {
-  //   exec('taskkill /IM TheIsleClient-Win64-Shipping.exe /F /T', execCallback) 
-  //   exec(`taskkill /IM TheIsle.exe /F /T`, execCallback);
-  //   exec('taskkill /PID ' + runningGame.pid + ' /T /F', execCallback);
-  //   gameRunning = false;
-  //   runningGame.kill();
-  // }, gameRuntTime[name]);
-// 
-// }
-
-// const waitForGame = (name: EGameNames, callforward: () => void, callback?: ()=> void) => {
-//   if (gameRunning) {
-//     setTimeout(() => waitForGame(name, callforward), 5000);
-//   } else {
-//     console.log("starting game: ", name, "for setup");
-//     callforward();
-//     startGameForConfig(name);
-//     if(callback){
-//       callback();
-//     }
-//   }
-// };
-
 if (
   store.store.loadedGame === "none" &&
-  store.store.legacyInstall &&
-  store.store.legacyAppData
+  store.store.legacyInstall
 ) {
-  checkOutAppData(EGameNames.legacy);
+  // checkOutAppData(EGameNames.legacy);
   store.set("loadedGame", EGameNames.legacy);
   store.store.loadedGame = EGameNames.legacy;
 }
 
 if (
   store.store.loadedGame === "none" &&
-  store.store.evrimaInstall &&
-  store.store.evrimaAppData
+  store.store.evrimaInstall
 ) {
-  checkOutAppData(EGameNames.legacy);
-  store.set("loadedGame", EGameNames.legacy);
-  store.store.loadedGame = EGameNames.legacy;
+  store.set("loadedGame", EGameNames.evrima);
+  store.store.loadedGame = EGameNames.evrima;
 }
-
-// check for spelling mistakes for evrima spelled: evirma
-if (
-  LOCAL_APP_DATA &&
-  fs.existsSync(path.join(LOCAL_APP_DATA, "TheIsleEvirma"))
-) {
-  fs.renameSync(
-    path.join(LOCAL_APP_DATA, "TheIsleEvirma"),
-    path.join(LOCAL_APP_DATA, "TheIsleEvrima")
-  );
-}
-
-// if appdata installs are false create the appdata folders for the game
-// if (LOCAL_APP_DATA && !store.store.legacyAppData) {
-//   waitForGame(EGameNames.legacy, () => {
-//     checkOutAppData(EGameNames.evrima);
-//   });
-// }
-
-// if (LOCAL_APP_DATA && !store.store.evrimaAppData) {
-//   waitForGame(EGameNames.evrima, () => {
-//     checkInAppData(EGameNames.legacy);
-//   },
-//   () => {
-//     setTimeout(() => checkOutAppData(EGameNames.evrima), 2000);
-//   })
-// }
